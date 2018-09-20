79917f7ef8646010a615d3944897a45f7a1cfcc3
Gercenio Xavier <gercenio@krooze.com.br>
2018-09-20 11:26:20 -0300

    inclusao de novos objetos

---
+++ TheChamaApp.Presentation.WebApi/Controllers/RellationshipEvaluatedToUpEvaluatedController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TheChamaApp.Application.IApplication;
+
+namespace TheChamaApp.Presentation.WebApi.Controllers
+{
+    
+    [Route("api/[controller]")]
+    public class RellationshipEvaluatedToUpEvaluatedController : BaseController
+    {
+        #region # Propriedades
+
+        private readonly IRellationshipEvaluatedToUpEvaluatedApplication _RellationshipEvaluatedToUpEvaluatedApplication;
+
+        #endregion
+
+        #region # Constructor
+
+        public RellationshipEvaluatedToUpEvaluatedController(IRellationshipEvaluatedToUpEvaluatedApplication rellationshipEvaluatedToUpEvaluatedApplication)
+        {
+            _RellationshipEvaluatedToUpEvaluatedApplication = rellationshipEvaluatedToUpEvaluatedApplication;
+        }
+
+        #endregion
+
+        #region # Actions
+
+        /// <summary>
+        /// Realiza a inclusão dos dados
+        /// </summary>
+        /// <param name="Entity"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Authorize("Bearer")]
+        public IActionResult Post([FromBody]Domain.Entities.RellationshipEvaluatedToUpEvaluated Entity)
+        {
+            
+            using (TheChamaApp.Service.EvaluatedBusiness.RellationshipEvaluatedToUpEvaluatedService RellationBO = new Service.EvaluatedBusiness.RellationshipEvaluatedToUpEvaluatedService(_RellationshipEvaluatedToUpEvaluatedApplication))
+            {
+                try
+                {
+                    RellationBO.Incluir(Entity, out Mensagem);
+                }
+                catch (Exception Ex)
+                {
+                    return BadRequest(Ex);
+                }
+                return Ok(Mensagem);
+            }
+        }
+
+        /// <summary>
+        /// Realiza a busca dos dados
+        /// </summary>
+        /// <param name="RellationshipEvaluatedToUpEvaluatedId"></param>
+        /// <returns></returns>
+        [HttpGet("{EvaluatedId}")]
+        [Authorize("Bearer")]
+        public IActionResult Get(int EvaluatedId)
+        {
+            using (TheChamaApp.Service.EvaluatedBusiness.RellationshipEvaluatedToUpEvaluatedService RellationBO = new Service.EvaluatedBusiness.RellationshipEvaluatedToUpEvaluatedService(_RellationshipEvaluatedToUpEvaluatedApplication))
+            {
+                try
+                {
+                    return Ok(RellationBO.ObterByEvaluatedId(EvaluatedId));
+                }
+                catch (Exception Ex)
+                {
+                    return BadRequest(Ex);
+                }
+                
+            }
+        }
+
+        /// <summary>
+        /// Realiza a exclusão de dados
+        /// </summary>
+        /// <param name="RellationshipEvaluatedToUpEvaluatedId"></param>
+        /// <returns></returns>
+        [HttpDelete("{RellationshipEvaluatedToUpEvaluatedId}")]
+        [Authorize("Bearer")]
+        public IActionResult Delete(int RellationshipEvaluatedToUpEvaluatedId)
+        {
+            using (TheChamaApp.Service.EvaluatedBusiness.RellationshipEvaluatedToUpEvaluatedService RellationBO = new Service.EvaluatedBusiness.RellationshipEvaluatedToUpEvaluatedService(_RellationshipEvaluatedToUpEvaluatedApplication))
+            {
+                try
+                {
+                    RellationBO.Excluir(RellationshipEvaluatedToUpEvaluatedId, out Mensagem);
+                }
+                catch (Exception Ex)
+                {
+                    return BadRequest(Ex);
+                }
+                return Ok(Mensagem);
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ TheChamaApp.Service/EvaluatedBusiness/RellationshipEvaluatedToUpEvaluatedService.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheChamaApp.Application.IApplication;
+using TheChamaApp.Domain.Entities;
+using System.Linq;
+
+namespace TheChamaApp.Service.EvaluatedBusiness
+{
+    public class RellationshipEvaluatedToUpEvaluatedService : Base.ServiceBase
+    {
+        #region # Propriedades
+        private readonly IRellationshipEvaluatedToUpEvaluatedApplication _IRellationshipEvaluatedToUpEvaluatedApplication;
+        #endregion
+
+        #region # Constructor
+        public RellationshipEvaluatedToUpEvaluatedService(IRellationshipEvaluatedToUpEvaluatedApplication rellationshipEvaluatedToUpEvaluatedApplication)
+        {
+            _IRellationshipEvaluatedToUpEvaluatedApplication = rellationshipEvaluatedToUpEvaluatedApplication;
+        }
+        #endregion
+
+        #region # Methods
+
+        /// <summary>
+        /// Realiza a inclusão de um relacionamento
+        /// </summary>
+        /// <param name="Entity"></param>
+        /// <param name="Mensagem"></param>
+        /// <returns></returns>
+        public RellationshipEvaluatedToUpEvaluated Incluir(RellationshipEvaluatedToUpEvaluated Entity, out string Mensagem)
+        {
+            Mensagem = string.Empty;
+            try
+            {
+                var EntityChk = _IRellationshipEvaluatedToUpEvaluatedApplication.GetAll().Where(m => m.EvaluatedId == Entity.EvaluatedId && m.UpEvaluatedId == Entity.UpEvaluatedId).ToList();
+                if (EntityChk.Count == 0)
+                {
+                    _IRellationshipEvaluatedToUpEvaluatedApplication.Add(Entity);
+                    Mensagem = Entity.RellationshipEvaluatedToUpEvaluatedId.ToString();
+                }
+            }
+            catch (Exception Ex)
+            {
+                Mensagem = string.Format("Erro:{0}", Ex.Message);
+            }
+            return Entity;
+        }
+
+        /// <summary>
+        /// Realiza a exclusão de um objeto
+        /// </summary>
+        /// <param name="RellationshipEvaluatedToUpEvaluatedId"></param>
+        /// <param name="Mensagem"></param>
+        public void Excluir(int RellationshipEvaluatedToUpEvaluatedId, out string Mensagem)
+        {
+            Mensagem = string.Empty;
+            try
+            {
+                var Original = _IRellationshipEvaluatedToUpEvaluatedApplication.GetAll().Where(m => m.RellationshipEvaluatedToUpEvaluatedId == RellationshipEvaluatedToUpEvaluatedId).Single();
+                if(Original != null)
+                {
+                    _IRellationshipEvaluatedToUpEvaluatedApplication.Remove(Original);
+                    Mensagem = "Done";
+                }
+            }
+            catch (Exception Ex)
+            {
+                Mensagem = string.Format("Erro:{0}", Ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Obtem uma lista de superior do subordinado
+        /// </summary>
+        /// <param name="EvaluatedId"></param>
+        /// <returns></returns>
+        public IEnumerable<Domain.Entities.RellationshipEvaluatedToUpEvaluated> ObterByEvaluatedId(int EvaluatedId)
+        {
+            return _IRellationshipEvaluatedToUpEvaluatedApplication.GetAll().Where(m => m.EvaluatedId == EvaluatedId).ToList();
+        }
+
+        #endregion
+    }
+}
