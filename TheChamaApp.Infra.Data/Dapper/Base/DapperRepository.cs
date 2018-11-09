b4106dd662df9286a9f790eb52d112c7253b35ae
gercenio@gmail.com <gercenio@gmail.com>
2018-11-09 10:50:27 -0200

    implementação de ajustes

--- TheChamaApp.Infra.Data/Dapper/Base/DapperRepository.cs
+++ TheChamaApp.Infra.Data/Dapper/Base/DapperRepository.cs
@@ -1,33 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Security.Cryptography;
 using System.Text;
 using Dapper;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
 
 namespace TheChamaApp.Infra.Data.Dapper.Base
 {
     public class DapperRepository
     {
+        #region # Propriedades
+
+        private readonly string Connection;
+
+        #endregion
+
         #region # Constructor
-        public DapperRepository() { }
+        public DapperRepository()
+        {
+            this.Connection = GetConnection();
+        }
         #endregion
 
         #region # Methods
         public IEnumerable<T> ExecuteSelect<T>(string Sql, object param = null)
         {
-
-            using (var con = new SqlConnection(this.Decrypt(ConfigurationManager.ConnectionStrings["Connection"].ToString())))
+            using (var con = new MySqlConnection(Connection))
             {
-                return con.Query<T>(Sql, param);
+                return con.Query<T>(Sql, param, commandType: CommandType.StoredProcedure);
             }
         }
 
         public T ExecuteSelectUnique<T>(string sql, object param = null)
         {
-            using (var con = new SqlConnection(this.Decrypt(ConfigurationManager.ConnectionStrings["Connection"].ToString())))
+            using (var con = new MySqlConnection(Connection))
             {
                 return con.QueryFirstOrDefault<T>(sql, param);
             }
@@ -59,6 +70,15 @@ namespace TheChamaApp.Infra.Data.Dapper.Base
             }
             return cipherText;
         }
+
+        private string GetConnection()
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+           return this.Decrypt(config.GetSection("ConnectionStrings:Connection").Value);        }
 
         #endregion
     }
