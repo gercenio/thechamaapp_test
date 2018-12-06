f9761361ca05ca555699b6a033831841c6c6fddd
Gercenio Silva (Contractor) <gxsilva@laureate.com.br>
2018-12-06 17:28:56 -0200

    inclusão de novo campo

--- TheChamaApp.Domain/ViewModel/IndividualGraficEvaluatedBody.cs
+++ TheChamaApp.Domain/ViewModel/IndividualGraficEvaluatedBody.cs
@@ -6,8 +6,8 @@ namespace TheChamaApp.Domain.ViewModel
 {
     public class IndividualGraficEvaluatedBody
     {
-        public decimal Media { get; set; }
-        public int Avaliado { get; set; }
+        public float Media { get; set; }
+        public string Avaliado { get; set; }
         public int NumeroSubordinados { get; set; }
         public string GrupoPergunta { get; set; }
         public string Formulario { get; set; }
